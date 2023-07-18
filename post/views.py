1869a141596434c041eb73d229482167cc2d2dea
nahowo <nahowo@naver.com>
2023-07-18 16:07:20 +0900

    post 기능 추기

--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import post
 
 # Create your views here.
+def post_list(request):
+    posts=post.objects.all()
+    return render(request, 'post/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+    Post=get_object_or_404(post,pk=pk)
+    return render(request,'post/post_detail.html',{'post':Post})
