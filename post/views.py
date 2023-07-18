ee18de769f0d9619fa29ca790aed0f1a01150d32
nahowo <nahowo@naver.com>
2023-07-18 17:00:49 +0900

    새 게시글 추가 버튼 생성

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import post
 
+
 # Create your views here.
 def post_list(request):
     posts=post.objects.all()
@@ -9,3 +10,9 @@ def post_list(request):
 def post_detail(request,pk):
     Post=get_object_or_404(post,pk=pk)
     return render(request,'post/post_detail.html',{'post':Post})
+
+def create_post(request):
+    return render(
+        request,
+        'post/post_create.html'
+    )
